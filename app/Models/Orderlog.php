d566d799233ea982535ea6ea14c697881520444a
ekrmabd <ekramhossainekram28@gmail.com>
2026-03-01 11:10:34 +0600

    make restue

--- app/Models/Orderlog.php
+++ app/Models/Orderlog.php
@@ -17,6 +17,6 @@ class Orderlog extends Model
     public function farmeritems()
     {
         //return $this->belongsTo(FarmerItem::class, 'farmeritem_id');
-        $this->hasMany(Farmeritem::class, 'farmeritem_id');
+        return $this->hasMany(Farmeritem::class, 'farmeritem_id');
     }
 }
