15592eb69c955a501f92a5939a7bfd2c37057ccc
ekrmabd <ekramhossainekram28@gmail.com>
2026-03-07 13:29:43 +0600

    ml

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1538,7 +1538,7 @@ class ApiController extends Controller
 
                 $radius = $request->radius;
 
-                $locations = Userinfo::whereIn('business_address')->pluck('businees_location')->toArray();
+                $locations = Userinfo::pluck('businees_location')->toArray();
 
                 return $locations;
             }
